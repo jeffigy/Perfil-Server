64ca9b89f30eff2a8b64ffbe2839b23271261e2d
jeffigy <ejeffydev@gmail.com>
2024-05-20 22:16:31 +0800

    feat: Added jwt to signup, as well as login, refresh, and logout features

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("../utils/config");
 
 const signup = async (req, res) => {
   const { name, email, password } = req.body;
@@ -29,15 +31,136 @@ const signup = async (req, res) => {
     role: "Patient",
   };
 
-  const user = await User.create(userObj);
+  const createdUser = await User.create(userObj);
 
-  if (user) {
-    res.status(201).json({ message: "Register Successfully" });
-  } else {
-    res.status(400).json({ message: "Received invalid user data" });
+  if (!createdUser) {
+    res.status(400).json({ message: "Received invalid data" });
   }
+
+  const accessToken = jwt.sign(
+    {
+      UserInfo: {
+        email: createdUser.email,
+        roles: createdUser.role,
+      },
+    },
+    config.ACCESS_TOKEN_SECRET,
+    { expiresIn: "15m" }
+  );
+
+  const refreshToken = jwt.sign(
+    { email: createdUser.email },
+    config.REFRESH_TOKEN_SECRET,
+    { expiresIn: "7d" }
+  );
+
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
+  res.json({ accessToken });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "email and password are required" });
+  }
+
+  const foundUser = await User.findOne({ email }).exec();
+
+  if (!foundUser || !foundUser.active) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const pwdMatch = bcrypt.compare(password, foundUser.password);
+
+  if (!pwdMatch) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const accessToken = jwt.sign(
+    {
+      UserInfo: {
+        email: foundUser.email,
+        roles: foundUser.role,
+      },
+    },
+    config.ACCESS_TOKEN_SECRET,
+    { expiresIn: "15m" }
+  );
+
+  const refreshToken = jwt.sign(
+    { email: foundUser.email },
+    config.REFRESH_TOKEN_SECRET,
+    { expiresIn: "7d" }
+  );
+
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
+  res.json({ accessToken });
+};
+
+const refresh = (req, res) => {
+  const cookies = req.cookies;
+
+  console.log(cookies);
+
+  if (!cookies?.jwt) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const refreshToken = cookies.jwt;
+
+  jwt.verify(
+    refreshToken,
+    config.REFRESH_TOKEN_SECRET,
+    async (error, decoded) => {
+      if (error) {
+        return res.status(403).json({ message: "Fobidden" });
+      }
+
+      const foundUser = await User.findOne({ email: decoded.email }).exec();
+
+      if (!foundUser) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+
+      const accessToken = jwt.sign(
+        {
+          UserInfo: {
+            email: foundUser.email,
+            role: foundUser.role,
+          },
+        },
+        config.ACCESS_TOKEN_SECRET,
+        { expiresIn: "15m" }
+      );
+
+      res.json({ accessToken });
+    }
+  );
+};
+
+const logout = (req, res) => {
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204); //No content
+  res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
+  res.json({ message: "Cookie cleared" });
 };
 
 module.exports = {
   signup,
+  login,
+  refresh,
+  logout,
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,9 @@
 const authRoutes = require("express").Router();
 const authController = require("../controllers/authController");
 
+authRoutes.route("/").post(authController.login);
 authRoutes.route("/signup").post(authController.signup);
+authRoutes.route("/refresh").get(authController.refresh);
+authRoutes.route("/logout").post(authController.logout);
 
 module.exports = authRoutes;
