2479425ebfd62273e00f8e5fc5480b1ccb503b38
jeffigy <ejeffydev@gmail.com>
2024-05-20 22:14:51 +0800

    refactor: Added ACCESS_TOKEN_SECRET and REFRESH_TOKEN_SECRET

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,16 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.NODE_ENV === "test" ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI;
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 
 module.exports = {
-    MONGODB_URI, 
-    PORT
-}
+  MONGODB_URI,
+  PORT,
+  ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET,
+};
