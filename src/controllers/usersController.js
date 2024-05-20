482ba41dbf8edc00a8036c82c8cb91bd12716cdd
jeffigy <ejeffydev@gmail.com>
2024-05-20 22:48:24 +0800

    feat: Added updateUser, and deleteUser functions

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,15 +1,110 @@
-const User = require('../models/User')
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-const getAllUsers = async  ( req, res ) => {
-    const users = await User.find({})
+const getAllUsers = async (_req, res) => {
+  const users = await User.find({ role: { $ne: "Patient" } });
 
-    if(users.length === 0 ) {
-        return res.status(400).json({message: "No users found"})
-    }
+  if (users.length === 0) {
+    return res.status(400).json({ message: "No users found" });
+  }
 
-    res.json(users)
-}
+  res.json(users);
+};
+
+const addUser = async (req, res) => {
+  const { email, name, password, role } = req.body;
+
+  if (!email || !name || !password || !role) {
+    return res
+      .status(400)
+      .json({ message: "name, email, and password are required" });
+  }
+
+  const duplicate = await User.findOne({ email })
+    .collation({ locale: "en", strength: 2 })
+    .lean()
+    .exec();
+
+  if (duplicate) {
+    return res.status(400).json({ message: "Duplicate Email" });
+  }
+
+  const hashedPwd = await bcrypt.hash(password, 10);
+
+  const userObj = {
+    email,
+    name,
+    password: hashedPwd,
+    role,
+  };
+
+  const newUser = await User.create(userObj);
+
+  if (newUser) {
+    return res.status(201).json({ message: "new user created" });
+  } else {
+    return res.status(400).json({ message: "Received invalid user data" });
+  }
+};
+
+const updateUser = async (req, res) => {
+  const { id, email, name, password, role, active } = req.body;
+
+  if (!id || !email || !name || !role || typeof active !== "boolean") {
+    return res
+      .status(400)
+      .json({ message: "all fields except password are required" });
+  }
+
+  const user = await User.findById(id).exec();
+
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  const duplicate = await User.findOne({ email })
+    .collation({ locale: "en", strength: 2 })
+    .lean()
+    .exec();
+
+  if (duplicate && duplicate?._id.toString() !== id) {
+    return res.status(409).json({ message: "Duplicate email" });
+  }
+  user.name = name;
+  user.email = email;
+  user.role = role;
+  user.active = active;
+
+  if (password) {
+    user.password = bcrypt.hash(password, 10);
+  }
+
+  const updatedUser = await user.save();
+
+  res.json({ message: `User ${updatedUser.email} updated` });
+};
+
+const deleteUser = async (req, res) => {
+  const { id } = req.body;
+
+  if (!id) {
+    return res.status(400).json({ message: "user id is required" });
+  }
+
+  const user = await User.findById(id).lean().exec();
+
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  const result = await user.deleteOne();
+
+  res.json({ message: `User with email ${result.email} has been deleted` });
+};
 
 module.exports = {
-    getAllUsers
-}
+  getAllUsers,
+  addUser,
+  updateUser,
+  deleteUser,
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,11 @@
-const userRoutes = require('express').Router()
-const usersController = require('../controllers/usersController')
+const userRoutes = require("express").Router();
+const usersController = require("../controllers/usersController");
 
-userRoutes.route('/').get(usersController.getAllUsers)
+userRoutes
+  .route("/")
+  .get(usersController.getAllUsers)
+  .post(usersController.addUser)
+  .patch(usersController.updateUser)
+  .delete(usersController.deleteUser);
 
-module.exports = userRoutes
+module.exports = userRoutes;
