b72bb6e830fb642c73eadf3b606252139ec90284
jeffigy <ejeffydev@gmail.com>
2024-06-22 22:56:15 +0800

    feat: Separated the User from the Patient, and implemented it to patientsController

--- src/controllers/patientsController.js
+++ src/controllers/patientsController.js
@@ -1,7 +1,7 @@
-const User = require("../models/User");
+const Patient = require("../models/Patient");
 
 const getAllPatients = async (_req, res) => {
-  const patients = await User.find({ role: "Patient" });
+  const patients = await Patient.find();
 
   if (patients.length == 0) {
     return res.status(400).json({ message: "No patients found" });
@@ -13,8 +13,7 @@ const getAllPatients = async (_req, res) => {
 const getAllPatientsByWorkplaceId = async (req, res) => {
   const { workId } = req.params;
 
-  const patients = await User.find({
-    role: "Patient",
+  const patients = await Patient.find({
     workplace: workId,
   }).exec();
 
---
+++ src/models/Patient.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+const { setToJSONTransform } = require("../utils/mongooseUtils");
+
+const patientSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      require: true,
+    },
+    name: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    role: {
+      type: String,
+      default: "Patient",
+    },
+    bday: {
+      type: Number,
+      require: false,
+    },
+    gender: {
+      type: String,
+      require: false,
+    },
+    civilStatus: {
+      type: String,
+      require: false,
+    },
+    fathersName: {
+      type: String,
+      require: false,
+    },
+    mothersName: {
+      type: String,
+      require: false,
+    },
+    ethnicity: {
+      type: String,
+      require: false,
+    },
+    nationality: {
+      type: String,
+      require: false,
+    },
+    workplace: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Workplace",
+      require: false,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+setToJSONTransform(patientSchema);
+
+const Patient = mongoose.model("Patient", patientSchema);
+
+module.exports = Patient;
