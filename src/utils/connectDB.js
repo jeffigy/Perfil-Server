6e84351565f7e22e82242906dc161204bcb779bb
jeffigy <ejeffydev@gmail.com>
2024-06-22 16:44:37 +0800

    refactor: Formatted the connectDB file

--- src/utils/connectDB.js
+++ src/utils/connectDB.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose')
-const config = require('./config')
+const mongoose = require("mongoose");
+const config = require("./config");
 
 const connectDB = async () => {
-    try {
-        mongoose.set("strictQuery", false)
-        await mongoose.connect(config.MONGODB_URI)
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(config.MONGODB_URI);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-module.exports = connectDB
+module.exports = connectDB;
