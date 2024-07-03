32e405594c8c2be4ff9423bb19dd5c409508b228
jeffigy <ejeffydev@gmail.com>
2024-07-03 23:32:08 +0800

    feat: Installed node-mailer, and modified register function and route to incorporate the email verification

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,9 @@
-const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 const config = require("../utils/config");
+const User = require("../models/User");
 const Patient = require("../models/Patient");
 
 const signup = async (req, res) => {
@@ -45,37 +47,61 @@ const signup = async (req, res) => {
     password: hashedPwd,
   };
 
-  const createdUser = await Patient.create(userObj);
+  const newUser = new Patient(userObj);
 
-  if (!createdUser) {
-    res.status(400).json({ message: "Received invalid data" });
-  }
+  newUser.verificationToken = crypto.randomBytes(32).toString("hex");
+  newUser.verificationTokenExpires = Date.now() + 3600000; // 1 hour
 
-  const accessToken = jwt.sign(
-    {
-      UserInfo: {
-        email: createdUser.email,
-        roles: createdUser.roles,
-      },
+  await newUser.save();
+
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: config.MAILER_USER,
+      pass: config.MAILER_PASSWORD,
     },
-    config.ACCESS_TOKEN_SECRET,
-    { expiresIn: "15m" }
-  );
-
-  const refreshToken = jwt.sign(
-    { email: createdUser.email },
-    config.REFRESH_TOKEN_SECRET,
-    { expiresIn: "7d" }
-  );
-
-  res.cookie("jwt", refreshToken, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "None",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
-  res.json({ accessToken });
+  const mailOptions = {
+    to: newUser.email,
+    from: `"Perfil" <${config.MAILER_USER}>`,
+    subject: "Email Verification",
+    text: `Please verify your email by clicking the link: \n\n 
+           http://localhost:5173/verify/${newUser.verificationToken}\n\n 
+           If you did not request this, please ignore this email.`,
+  };
+
+  await transporter.sendMail(mailOptions);
+  res.status(200).json({
+    message: "A verification email has been sent to " + newUser.email + ".",
+  });
+};
+
+const verify = async (req, res) => {
+  const { token } = req.params;
+  console.log("Received token:", token);
+  const user = await Patient.findOne({
+    verificationToken: token,
+    verificationTokenExpires: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    console.log("Token is invalid or has expired.");
+    return res
+      .status(400)
+      .json({ message: "Verification token is invalid or has expired." });
+  }
+
+  user.isVerified = true;
+  user.verificationToken = undefined;
+  user.verificationTokenExpires = undefined;
+
+  await user.save();
+
+  console.log("User verified successfully.");
+  return res
+    .status(200)
+    .json({ message: "Your email has been verified successfully!" });
 };
 
 const login = async (req, res) => {
@@ -183,6 +209,7 @@ const logout = (req, res) => {
 
 module.exports = {
   signup,
+  verify,
   login,
   refresh,
   logout,
--- src/utils/config.js
+++ src/utils/config.js
@@ -10,6 +10,8 @@ const MONGODB_URI =
 const CORS_WHITELIST = process.env.CORS_WHITELIST
   ? process.env.CORS_WHITELIST.split(",")
   : [];
+const MAILER_USER = process.env.MAILER_USER;
+const MAILER_PASSWORD = process.env.MAILER_PASSWORD;
 
 module.exports = {
   MONGODB_URI,
@@ -17,4 +19,6 @@ module.exports = {
   ACCESS_TOKEN_SECRET,
   REFRESH_TOKEN_SECRET,
   CORS_WHITELIST,
+  MAILER_USER,
+  MAILER_PASSWORD,
 };
